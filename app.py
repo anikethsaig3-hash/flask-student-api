abfa1390901f8cea1b389d79d955dbc8f632ab68
anikethsaig3-hash <anikethsaig3@gmail.com>
2026-07-20 11:54:07 +0530

    Change Flask environment to production for main app

--- app.py
+++ app.py
@@ -70,7 +70,6 @@ def create_app(config_name='development'):
         }), 200
     
     return app
-
+app = create_app(os.getenv('FLASK_ENV', 'production'))
 if __name__ == '__main__':
-    app = create_app(os.getenv('FLASK_ENV', 'development'))
     app.run(debug=True, host='0.0.0.0', port=5000)
